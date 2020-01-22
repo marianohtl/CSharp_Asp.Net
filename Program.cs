08c5d412c0389b204e5217666150acbbe4947dec
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-22 13:23:44 -0300

    adicionando exercicios do capítulo 3

--- Program.cs
+++ Program.cs
@@ -6,22 +6,20 @@ namespace O.O.CSharpExercises
     {
         static void Main(string[] args)
         {
-            TestBasicOO ex = new TestBasicOO();
+            Cap3Ex01 ex = new Cap3Ex01();
             bool continuar = true;
             while (continuar == true)
             {
                 string opcao;
                 Console.WriteLine("############################################################");
-                Console.WriteLine("Algorítimo Resultante de um Teste de Lógica de Programação");
+                Console.WriteLine("          Lista de Exercícios 01 - Capítulo 3");
                 Console.WriteLine("############################################################");
                 Console.WriteLine("Escolha o exercício:");
-                Console.WriteLine("#1");
-                Console.WriteLine("#2");
-                Console.WriteLine("#3");
-                Console.WriteLine("#4");
-                Console.WriteLine("#5");
-                Console.WriteLine("#6");
-                Console.WriteLine("#0");
+                Console.WriteLine("#1 - Soma");
+                Console.WriteLine("#2 - Diferença");
+                Console.WriteLine("#3 - Calculando Salário");
+                Console.WriteLine("#4 - Calculando Áreas Distintas");
+                Console.WriteLine("#0 - Sair");
                 Console.WriteLine("############################################################");
                 Console.Write("#");
                 opcao = Console.ReadLine();
@@ -29,41 +27,28 @@ namespace O.O.CSharpExercises
                 {
                     case "1":
                         Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #1 - Calculando dois Preços");
+                        Console.WriteLine("Exercício #1 - Soma");
                         Console.WriteLine("---------------------------------------");
-                        ex.CalcPreco();
+                        ex.Soma();
                         break;
 
                     case "2":
                         Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #2 - A Área de um Círculo");
+                        Console.WriteLine("Exercício #2 - Diferença");
                         Console.WriteLine("---------------------------------------");
-                        ex.CircleArea();
+                        ex.Diferenca();
                         break;
                     case "3":
                         Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #3 - Tabela de Lanches e Bebidas");
+                        Console.WriteLine("Exercício #3 - Calculando Salário");
                         Console.WriteLine("---------------------------------------");
-                        ex.Lanches();
+                        ex.Salario();
                         break;
                     case "4":
                         Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #4 - Verificar Senha");
+                        Console.WriteLine("Exercício #4 - Calculando Área Distintas");
                         Console.WriteLine("---------------------------------------");
-
-                        ex.EquSegundoGrau();
-                        break;
-                    case "5":
-                        Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #5 - Verificar Senha");
-                        Console.WriteLine("---------------------------------------");
-                        ex.VerificaSenha();
-                        break;
-                    case "6":
-                        Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Exercício #6 - Intervalo de 10 à 20");
-                        Console.WriteLine("---------------------------------------");
-                        ex.Intervalos();
+                        ex.CalcAreas();
                         break;
                     case "0":
                         Console.WriteLine("Goodbye :)");
