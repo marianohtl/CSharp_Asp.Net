73ace5497bc2ec01a38f65b36cc5aa3cffec9b47
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-29 16:39:17 -0300

    Adicionando exercício Circunferência com métodos estáticos no program.

---
+++ Cap4_ex06_a/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap4_ex06_a
+{
+    class Program
+    {
+        //vamos declarar a constante PI e ela será estática, pois será usada em outra classe estática
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        //Aqui não necessitamos do encapsulador public, pois estamos trabalhando dentro da mesma classe,
+        //porém para que essa função funcione dentro de MAIN precisamos usar o static, pois o Main é uma função statica, 
+        //e não podemos chamar uma função estática da mesma classe dentro de uma função estática  
+
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume (double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
