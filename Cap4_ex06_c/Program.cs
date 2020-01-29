332b96cc6c6e568502134ffa6853d6a9a64790fb
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-29 16:44:08 -0300

    Adicionando exercício circunferência sem intancias objeto calculadora (chamando métodos direto pela classe) _ Membros Estáticos

---
+++ Cap4_ex06_c/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap4_ex06_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui temos a chamada direto da classe Calculadora (não mais do objeto da classe)
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
