b27fb46a6c62db4f060c53a0d164b8387a7c076d
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-29 13:58:47 -0300

    Adicionando exercício 03 - Retângulo Com O.O

---
+++ Cap4ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap4ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            Console.Write("Digite a Largura do Retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            Console.Write("Digite a Altura do Retângulo: ");
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ÁREA: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO: " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
