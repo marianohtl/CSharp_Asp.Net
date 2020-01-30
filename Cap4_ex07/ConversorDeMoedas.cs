03876a502103409554dc4469d3663db380eb5c05
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-29 22:12:00 -0300

    Adicionando exercicio referente a Membros Estático - Conversor de Moedas

---
+++ Cap4_ex07/ConversorDeMoedas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap4_ex07
+{
+    class ConversorDeMoedas
+    {
+        public static double Cotacao;
+
+        public static double Dolares;
+
+        public static double Converter()
+        {
+            double convertido;
+            convertido = Dolares * Cotacao;
+            convertido = convertido + (convertido * 0.06);
+            return convertido;
+        }
+    }
+}
---
+++ Cap4_ex07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap4_ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoedas.Cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoedas.Dolares = double.Parse(Console.ReadLine());
+            Console.WriteLine("O valor a ser pago em reais = "+ConversorDeMoedas.Converter().ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
