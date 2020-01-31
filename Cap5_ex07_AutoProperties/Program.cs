e552c096230133a0125ae760484fab025a33424a
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-30 21:57:42 -0300

    adicionando auto-properties

---
+++ Cap5_ex07_AutoProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cap5_ex07_AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Apple",50.55,9);
+            p.Nome = "A";
+            Console.WriteLine(p);
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+            
+        }
+    }
+}
