d1faa8d5d8ea4f7fe9e06064afe7df70bdf6bfbc
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-30 21:27:39 -0300

    adicionando exemplo de propertie com get e set e método private

---
+++ Cap5_ex06_Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cap5_ex06_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Banana",500.00,10);
+            Console.WriteLine(p);
+            p.Nome = "Bananas";
+            p.Nome = "B"; //a propertie barrou o B
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
